65498be661d01a28542542b1d9f48d0d702881c3
Lucia Vasquez <100252668+LuciaVasquez96@users.noreply.github.com>
2024-09-22 00:03:57 -0300

    Add files via upload

---
+++ plataformaDisco/tours.js
@@ -0,0 +1,15 @@
+let nombre = prompt("¡Hola!¿Cuál es tu nombre?");
+        
+let edad = prompt("¿Cuántos años tenés?");
+    
+  alert("Bienvenido/a " + nombre + " de " + edad +"¿Te gustaría adquirir tus entradas?")
+
+
+//let nombre = prompt("¿cual es tu nombre?").toUpperCase();
+
+//while(nombre.length<3){
+
+  //  nombre= prompt("Por favor ingresa un nombre que tenga minimo 3 letras");
+
+//}
+//Me falta terminar
